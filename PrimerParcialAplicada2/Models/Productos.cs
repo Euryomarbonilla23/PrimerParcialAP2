409a1baae0976370434b38deaada3e7ca21ad81a
Eury Omar <Eury Omar@LAPTOP-35F7H0GD>
2021-06-02 17:30:31 -0500

    Productos

---
+++ PrimerParcialAplicada2/Models/Productos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcialAplicada2.Models
+{
+    public class Productos
+    {
+    }
+}
